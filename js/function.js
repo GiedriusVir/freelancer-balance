3e3579c08d4754333008c793caedadfa4f21d4d2
GiedriusVir <virsilasg@gmail.com>
2019-10-03 23:15:11 +0300

    Render full table info

--- js/function.js
+++ js/function.js
@@ -1,15 +1,27 @@
 function renderTable(data) {
     let HTML = '';
-
+    let balance = 0;
     for (let i = 0; i < data.length; i++){
         let item = data[i];
+
+        if (item.income && item.expense) {
+            balance = item.income - item.expense +'.00 Eur';
+        }
+        if (!item.income) {
+            balance = -item.expense +'.00 Eur';
+            item.income = '-';
+        }
+        if (!item.expense) {
+            balance = item.income +'.00 Eur';
+            item.expense = '-';
+        }
 
         HTML += `<div class="table-row">
                     <div class="cell">${i+1}</div>
                     <div class="cell">${item.month}</div>
-                    <div class="cell">${item.income}.00 Eur</div>
+                    <div class="cell">${item.income}</div>
                     <div class="cell">${item.expense}</div>
-                    <div class="cell">${item.income - item.expense}.00 Eur</div>
+                    <div class="cell">${balance}</div>
                 </div>`;
     }
  
@@ -19,12 +31,9 @@ function renderTable(data) {
 function renderMonths(data) {
     let monthNum = document.querySelectorAll('.table-row .cell:nth-child(2)');
 
-    for (let i = 0; i < data.length; i++){
-        monthNum.innerHTML = data[i];
-        
-        // console.log(monthNum[i].innerHTML);
-    }
-    
+    monthNum.forEach(item => {
+        item.innerHTML = data[item.innerHTML-1];
+    });
 
     return;
 }
